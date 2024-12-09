887c2d1b298da52dca02da38c7ba16547b8b567b
jose rodriguez <joserockabarcelona@gmail.com>
2024-12-09 16:41:48 +0100

    refactored loadItems() function

--- src/client/script.js
+++ src/client/script.js
@@ -1,64 +1,56 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let list = [];
-    let listForm = document.getElementById('listForm');
+document.addEventListener("DOMContentLoaded", function () {
+  let list = [];
+  let listForm = document.getElementById("listForm");
 
-    listForm.addEventListener('submit', saveItem);
+  listForm.addEventListener("submit", saveItem);
 
-    function saveItem(event) {
-        event.preventDefault();
-        console.log('Formulario enviado'); 
+  function saveItem(event) {
+    event.preventDefault();
+    console.log("Formulario enviado");
 
-        const itemInput = document.getElementById('inputText');
-        const item = itemInput.value.trim();
+    const itemInput = document.getElementById("inputText");
+    const item = itemInput.value.trim();
 
-        
-        if (item !== '') {
-            fetch('http://localhost:5000/items', { 
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({item}),
-            })
-                .then(response => response.json())
-                .then(function (data) {
-                    if (data.success) {
-                        itemInput.value = '';
-                        loadItems();  
-                    }        
-                });
-        }
-    }
-
-
-    function listView() {
-        const listElement = document.getElementById('list');
-        listElement.innerHTML = ''; 
-
-        list.forEach(function(item) {
-            const li = document.createElement('li');
-            li.textContent = item;
-            listElement.appendChild(li);
+    if (item !== "") {
+      fetch("http://localhost:5000/items", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ item }),
+      })
+        .then((response) => response.json())
+        .then(function (data) {
+          if (data.success) {
+            itemInput.value = "";
+            loadItems();
+          }
         });
     }
+  }
 
-    //
-    function loadItems() {
-        fetch('http://localhost:5000/items')
-            .then(response => response.json())
-            .then(function (data) {
-                for(let i = 0; i < data.length; i++){
-                    let item = data[i];
-                    list = data.push(item.name);
-                }
-                listView();
-            });
-    }
+  function listView() {
+    const listElement = document.getElementById("list");
+    listElement.innerHTML = "";
 
-    loadItems();
+    list.forEach(function (item) {
+      const li = document.createElement("li");
+      li.textContent = item;
+      listElement.appendChild(li);
+    });
+  }
+
+  //
+  function loadItems() {
+    fetch("http://localhost:5000/items")
+      .then((response) => response.json())
+      .then(function (data) {
+        list = []; 
+        for (let i = 0; i < data.length; i++) {
+          list.push(data[i].name);
+        }
+
+        listView();
+      });
+  }
+
+  loadItems();
 });
-
-
-
-
-
-
-
--- src/server/server.js
+++ src/server/server.js
@@ -1,78 +1,69 @@
-import { createServer } from 'node:http';
-import * as mysql from 'mysql2';
+import { createServer } from "node:http";
+import * as mysql from "mysql2";
 
 const PORT = 5000;
 
 // Crear una conexión a la base de datos
 const connection = mysql.createConnection({
-  host: 'localhost',     
-  user: 'root',          
-  password: '00reset',  
-  database: 'db_list' 
+  host: "localhost",
+  user: "root",
+  password: "00reset",
+  database: "db_list",
 });
-  
+
 // Conectar a la base de datos
 connection.connect((err) => {
   if (err) {
-    console.error('Error de conexión: ' + err.stack);
+    console.error("Error de conexión: " + err.stack);
     return;
   }
 
-  console.log('Conectado a la base de datos');
+  console.log("Conectado a la base de datos");
 });
-
-
-
 
 const server = createServer((req, res) => {
   // res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader('Access-Control-Allow-Origin', 'localhost:3000');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Content-Type", "application/json");
 
   console.log({ method: req.method, url: req.url });
 
-
-
-
-// GET  
-  if (req.method === 'GET' && req.url === '/items') {
-    connection.query('SELECT * FROM items', function (error, results, fields) {
+  // GET
+  if (req.method === "GET" && req.url === "/items") {
+    connection.query("SELECT * FROM items", function (error, results, fields) {
       res.end(JSON.stringify(results, null, 2));
     });
 
     return;
   }
 
+  // POST
+  if (req.method === "POST" && req.url === "/items") {
+    let body = "";
 
-
-// POST
-  if (req.method === 'POST' && req.url === '/items') {
-    let body = '';
-
-    req.on('data', (chunk) => {
-        body += chunk;
+    req.on("data", (chunk) => {
+      body += chunk;
     });
 
-    req.on('end', () => {
-        const { item } = JSON.parse(body);
-        connection.query('INSERT INTO items (name) VALUES (?)', [item], (error, results) => {
-            res.writeHead(201);
-            res.end(JSON.stringify({ success: true, id: results.insertId }));
-        });
+    req.on("end", () => {
+      const { item } = JSON.parse(body);
+      connection.query(
+        "INSERT INTO items (name) VALUES (?)",
+        [item],
+        (error, results) => {
+          res.writeHead(201);
+          res.end(JSON.stringify({ success: true, id: results.insertId }));
+        }
+      );
     });
 
     return;
   }
 
   res.end();
-
-
-
-
 });
 // starts a simple http server locally on port 5000
-server.listen(PORT, '127.0.0.1', () => {
+server.listen(PORT, "127.0.0.1", () => {
   console.log(`Listening on 127.0.0.1:${PORT}`);
 });
-
